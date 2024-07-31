65712677a564279c19bee9273c2c6257d8c03934
Mypilord <161896782+Mypilord@users.noreply.github.com>
2024-07-30 23:33:47 -0400

    finished scoring system and started working on autoclicker

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-let score = 0;
-function score() {
-    click = score + 1
-    document.getElementById("potion").innerhtml = score + 1; 
+let score = 0; 
+let autoclick = false; 
+let setInterval = 
+function scoreCount() {
+    score= score +1
+    document.getElementById('score').textContent = score;
 }
+
+function autoclicker() {
+    if (score >= 30) {
+        autoclick = true;
+         setInterval (() => {
+            if (autoclick) {
+                score = score + 1;
+                document.getElementById('score').textContent = score;
+            }
+        }, 3000); 
+      }
+    } 
