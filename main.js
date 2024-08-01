a138036cce0086cc9c4afc5b02aab5d25bc046c3
Mypilord <161896782+Mypilord@users.noreply.github.com>
2024-08-01 09:35:39 -0400

    Deleted the ai function and made my own function, fixed some loose ends.
    still a work in progress but I do wanna move onto a new project for now.

--- main.js
+++ main.js
@@ -1,34 +1,49 @@
 let score = 0; 
 let autoclick = false; 
+let doubleclick = false; 
 
-function scoreCount() {
-    score = score +1
+function scoreCount() { 
+    if (doubleclick == false) {
+    score = score + 1
     document.getElementById('score').textContent = score;
+} else if (doubleclick == true) {
+const button2 = document.getElementById('doubleclicker');
+button2.textContent = 'Sold out!';
+score = score + 2
+document.getElementById('score').textContent = score;
 }
+} 
+
+function doubleclicker() {
+    if (score >= 1000) {
+        score = score - 1000;
+        doubleclick = true; 
+      }
+      else {
+        alert ("not enough money honey, you broke son"); 
+      }
+    } 
 
 
-
- 
-function autoclicker() {
-        if (score >= 30) {
-            score = score - 30; 
+    
+    function autoclicker() {
+        if (score >= 200) {
+            score = score - 200; 
         autoclick = true;
          setInterval (() => {
             if (autoclick) {
                 score = score + 1;
-                document.getElementById('score').textContent = score;
+                document.getElementById('score').textContent = score; 
+                const button1 = document.getElementById('autoclicker');
+                button1.textContent = 'Sold out!';
             }
-          
+
         }, 5000); 
       }
+      else {
+        alert ("broke bud, you need money."); 
+      }
     } 
-
-    function toggleDropdown() {
-        let dropdown = document.getElementById("dropdownmenu");
-        if (dropdown.style.display === "block") {
-            dropdown.style.display = "none";
-        } else {
-            dropdown.style.display = "block";
-        }
-    }
-   
+    
+    
+ 
